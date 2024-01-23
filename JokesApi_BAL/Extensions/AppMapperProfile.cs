d49d502133dfbd82cedc578c4677407e2b635d6d
basileia <jane.novak@centrum.cz>
2024-01-23 22:14:27 +0100

    Automapper extension

---
+++ JokesApi_BAL/Extensions/AppMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using JokesApi_BAL.Models;
+using JokesApi_DAL.Entities;
+
+namespace JokesApi_BAL.Extensions
+{
+    public class AppMapperProfile : Profile
+    {
+        public AppMapperProfile()
+        {
+            CreateMap<Category, CategoryModel>().ReverseMap();
+            CreateMap<Joke, JokeModel>().ReverseMap();
+
+        }
+    }
+}
