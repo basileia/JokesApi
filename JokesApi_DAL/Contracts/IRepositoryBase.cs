dabbf30d7cdee14a93247ddd3d95a235ff07fefc
basileia <jane.novak@centrum.cz>
2024-03-11 22:01:07 +0100

    Base Repository interface

---
+++ JokesApi_DAL/Contracts/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace JokesApi_DAL.Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        Task<T> Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        bool Exists(int id);
+    }
+}
