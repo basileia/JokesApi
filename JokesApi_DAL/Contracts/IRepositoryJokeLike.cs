2b69c1b94e06c3b36039cb21dc5de3c10d1eb41c
basileia <jane.novak@centrum.cz>
2025-04-29 13:58:26 +0200

    Add JokeLike repository interface with domain-specific methods

---
+++ JokesApi_DAL/Contracts/IRepositoryJokeLike.cs
@@ -0,0 +1,10 @@
+﻿using JokesApi_DAL.Entities;
+
+namespace JokesApi_DAL.Contracts
+{
+    public interface IRepositoryJokeLike : IRepositoryBase<JokeLike>
+    {
+        bool HasUserLikedJoke(int jokeId, string ipAddress);        
+        int GetLikeCount(int jokeId);
+    }
+}
